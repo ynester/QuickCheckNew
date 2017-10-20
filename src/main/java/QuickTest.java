4eb7ed8d0317e755dc88294c03868a344a922522
Yana Nester <ynester@convera.com>
2017-10-20 14:24:14 -0700

    Initial commit

---
+++ src/main/java/QuickTest.java
@@ -0,0 +1,14 @@
+import io.github.bonigarcia.wdm.ChromeDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+
+public class QuickTest {
+    public static void main(String[] args) {
+        ChromeDriverManager.getInstance().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.google.com/");
+        System.out.println("Test completed");
+        driver.quit();
+    }
+}
